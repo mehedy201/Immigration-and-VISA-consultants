a5f54e416bf93a4a8c15b73ec492e5699471e887
mehedy201 <mehedy643@gmail.com>
2022-04-18 12:37:02 +0600

    added new imgage in aboutImigration components

--- src/Pages/Home/AboutImigration/AboutImigration.js
+++ src/Pages/Home/AboutImigration/AboutImigration.js
@@ -1,24 +1,26 @@
 import React from 'react';
 import './AboutImigration.css'
-import icon from '../../../images/logo.png'
+import student from '../../../images/student.png'
+import worker from '../../../images/worker.png'
+import business from '../../../images/buisness.png'
 
 const AboutImigration = () => {
     return (
             <div className="container">
                 <div className="content_info d-flex justify-content-between">
                     <div className="student_visa p-3">
-                        <img width='80px'src={icon} alt="" />
+                        <img width='70px'src={student} alt="" />
                         <h4 className='title_color'>Student Visa</h4>
                         <p className='font_style'>We are Conected to multiple University in Europe like United Status, Canada, etc. It's very importent for student. our agency is best for education visa...</p>
                     </div>
                     <div className="worker_visa p-3">
-                        <img width='80px'src={icon} alt="" />
+                        <img width='70px'src={worker} alt="" />
                         <h4 className='title_color'>Worker Visa</h4>
                         <p className='font_style'>If you Work to europe country. Then I will give you working visa is very short time. We are connected to multiple company in europe...</p>
                     </div>
                     <div className="turist_vsa p-3">
-                        <img width='80px'src={icon} alt="" />
-                        <h4 className='title_color'>Turist Visa</h4>
+                        <img width='70px'src={business} alt="" />
+                        <h4 className='title_color'>Tourist Visa</h4>
                         <p className='font_style'>Jurney is good part of life. If you tour multiple country then you will gain perfect knolwge in every country. We have multiple staf in evry country...</p>
                     </div>
                 </div>
--- src/Pages/Home/Packages/Packages.js
+++ src/Pages/Home/Packages/Packages.js
@@ -4,7 +4,7 @@ import './Packages.css'
 
 const pakages = [
     {id: 1, name: 'Education Visa', price: 99, des: 'If you want to Education Visa Then our agency is a best for you becouse, We are specily provide student visa with low cost.'},
-    {id: 2, name: 'Turist & Visiting Visa', price: 199, des: 'Ture is a best part of man life. We are connected to world best turist country. we have multiple office in evry turist country.'},
+    {id: 2, name: 'Tourist & Visiting Visa', price: 199, des: 'Tour is a best part of man life. We are connected to world best turist country. we have multiple office in evry turist country.'},
     {id: 3, name: 'Working Visa', price: 155, des: 'Working visa is a very importent part in visa. Poor pepole need working visa so we are connect to word best working country.'}
 ]
 
